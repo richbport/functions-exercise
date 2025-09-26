eacc05d217e06f3818616726f92332b5e8a44a4a
Rich <richbport@yahoo.com>
2025-09-26 18:34:54 -0400

    notes for script.js

--- Functions Exercise 1/script.js
+++ Functions Exercise 1/script.js
@@ -2,14 +2,14 @@
 //          Declare code once, use it whenever you want
 //          Call the function to execute that code
 
-function happyBirthday(username, age) {
-  console.log("Happy birthday to you");
-  console.log("Happy birthday to you");
-  console.log(`Happy birthday dear ${username}`);
-  console.log("Happy birthday to you");
-  console.log(`You are ${age} years old`);
+function happyBirthday(username, age) { // Parameters username, and age, within the declaration
+  console.log("Happy birthday to you!");
+  console.log("Happy birthday to you!");
+  console.log(`Happy birthday dear ${username}!`);
+  console.log("Happy birthday to you!");
+  console.log(`You are ${age} years old!`);
 }
 
-happyBirthday("Rich", 54);
+happyBirthday("Rich", 54); // calling the function using arguments "Rich", and 54
 happyBirthday("Donny", 25);
 happyBirthday("Marie", 35);
---
+++ Functions Exercise 1/script2.js
@@ -0,0 +1,7 @@
+// function - a section of reusable code
+//          Declare code once, use it whenever you want
+//          Call the function to execute that code
+
+function add(x, y){
+    let result
+}
