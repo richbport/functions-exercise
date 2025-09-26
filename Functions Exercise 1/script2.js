4a6423bf4c3001b787fc47d06b5fa96c022eb3fa
Rich <richbport@yahoo.com>
2025-09-26 18:54:58 -0400

    multiply & divide functions

--- Functions Exercise 1/script2.js
+++ Functions Exercise 1/script2.js
@@ -2,10 +2,28 @@
 //          Declare code once, use it whenever you want
 //          Call the function to execute that code
 
-function add(x, y){
-    let result = x + y;
-    return result;
+// function add(x, y){
+//     let result = x + y;
+//     return result;
+// }
+
+function add(x, y) {
+  return x + y; // works same as above
 }
 
-let answer = add(2, 3);
-console.log(answer)
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+// let answer = subtract(2, 3);
+// console.log(answer)
+
+console.log(divide(2, 3)); // no variable called 'answer' to store it in, just console.logging it
