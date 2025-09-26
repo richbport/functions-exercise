4f26611c80aabee46b490292978667af8bdede30
Rich <richbport@yahoo.com>
2025-09-26 16:51:05 -0400

    function happyBirthday

---
+++ Functions Exercise 1/script.js
@@ -0,0 +1,10 @@
+// function - a section of reusable code
+
+function happyBirthday() {
+    console.log("Happy birthday to you");
+    console.log("Happy birthday to you");
+    console.log("Happy birthday dear you");
+    console.log("Happy birthday to you");
+}
+
+happyBirthday()
