37dbe619d38fd1f602f16df66e9a734f5cc45202
Rich <richbport@yahoo.com>
2025-09-26 16:55:06 -0400

    parameters and arguments

--- Functions Exercise 1/script.js
+++ Functions Exercise 1/script.js
@@ -1,10 +1,13 @@
 // function - a section of reusable code
+//          Declare code once, use it whenever you want
+//          Call the function to execute that code
 
-function happyBirthday() {
+function happyBirthday(username, age) {
     console.log("Happy birthday to you");
     console.log("Happy birthday to you");
-    console.log("Happy birthday dear you");
+    console.log(`Happy birthday dear ${username}`);
     console.log("Happy birthday to you");
+    console.log(`You are ${age} years old`);
 }
 
-happyBirthday()
+happyBirthday("Rich", 54)
