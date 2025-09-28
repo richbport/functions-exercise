721511a83811d98d704c1c33ddc63d78088317b7
Rich <richbport@yahoo.com>
2025-09-27 20:17:31 -0400

    answer = (isEven())

--- Functions Exercise 1/script2.js
+++ Functions Exercise 1/script2.js
@@ -7,26 +7,29 @@
 //     return result;
 // }
 
-function add(x, y) {
-  return x + y; // works same as above
-}
+// let answer = add(2, 3);
+// console.log(answer);
 
-function subtract(x, y) {
-  return x - y;
-}
+// function add(x, y) {
+//   return x + y; // works same as above
+// }
 
-function multiply(x, y) {
-  return x * y;
-}
+// function subtract(x, y) {
+//   return x - y;
+// }
 
-function divide(x, y) {
-  return x / y;
-}
+// function multiply(x, y) {
+//   return x * y;
+// }
 
-// let answer = subtract(2, 3);
-// console.log(answer)
+// function divide(x, y) {
+//   return x / y;
+// }
 
-// console.log(divide(2, 3)); // no variable called 'answer' to store it in, just console.logging it
+// // let answer = subtract(2, 3);
+// // console.log(answer)
+
+// // console.log(divide(2, 3)); // no variable called 'answer' to store it in, just console.logging it
 
 function isEven(number) {
   if (number % 2 === 0) {
@@ -40,18 +43,21 @@ function isEven(number) {
   return number % 2 === 0 ? true : false; //ternary operator
 }
 
-function isValidEmail(email) {
-  if (email.includes("@")) {
-    return true;
-  } else {
-    return false;
-  }
-}
+let answer = (isEven(2))
+console.log(answer)
 
-function isValidEmail(email) {
-    return email.includes("@") ? true : false;  //ternary operator equivalent
-}
+// function isValidEmail(email) {
+//   if (email.includes("@")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-console.log(isValidEmail("rich@yahoo.com"));
-console.log(isValidEmail("richyahoo.com"));
-console.log(isValidEmail("MarkZuckerborg@yahoo.com"));
+// function isValidEmail(email) {
+//     return email.includes("@") ? true : false;  //ternary operator equivalent
+// }
+
+// console.log(isValidEmail("rich@yahoo.com"));
+// console.log(isValidEmail("richyahoo.com"));
+// console.log(isValidEmail("MarkZuckerborg@yahoo.com"));
