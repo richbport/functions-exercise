a4b713f89f7609e6000fa8e0a073f432dc91ecfe
Rich <richbport@yahoo.com>
2025-09-29 11:00:31 -0400

    function1, function2

--- Functions Exercise 1/script3.js
+++ Functions Exercise 1/script3.js
@@ -1,24 +1,24 @@
 // variable scope - where a variable is recognized
 //                  and accessible (local vs global)
 
-let x = 3;
+// let x = 3;
 
-function1();
-
-// function function1() {
-//     let x = 1;
-//     console.log(x);
-// }
-
-// function function2() {
-//     let x = 2;
-//     console.log(x);
-// }
+function2();
 
 function function1() {
+    let x = 1;
     console.log(x);
 }
 
 function function2() {
+    let x = 2;
     console.log(x);
 }
+
+// function function1() {
+//     console.log(x);
+// }
+
+// function function2() {
+//     console.log(x);
+// }
